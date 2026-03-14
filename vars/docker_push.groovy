aef38a19d9cacfb7bd4bbe7940b82b8fcc85f751
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-14 19:00:41 +0530

    created the docker push lib

---
+++ vars/docker_push.groovy
@@ -0,0 +1,7 @@
+def call(String Project, String ImageTag, String dockerHubPass){
+    echo "Pushing to docker hub"
+    withCredentials([usernamePassword(credentialsId: "dockerHub",passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")])
+    sh "docker tag my-note-app ${env.dockerHubUser}/my-note-app:latest"
+    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+    sh "docker push ${env.dockerHubUser}/my-note-app:latest"
+
