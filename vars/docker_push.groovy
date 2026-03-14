fa44a1da0a5d4517ae3ea6c9a9db1240c79681b9
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-14 19:08:49 +0530

    Fixed the curly braces

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,8 @@
 def call(String Project, String ImageTag, String dockerHubPass){
     echo "Pushing to docker hub"
-    withCredentials([usernamePassword(credentialsId: "dockerHub",passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")])
+    withCredentials([usernamePassword(credentialsId: "dockerHub",passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]){
     sh "docker tag my-note-app ${env.dockerHubUser}/my-note-app:latest"
     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
     sh "docker push ${env.dockerHubUser}/my-note-app:latest"
-
+    }
 }
