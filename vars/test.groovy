fae2bfabcb8e12839e428801f363c21057378c71
mr-mayu-mane <mayurmane2577@gmail.com>
2026-03-15 13:13:35 +0530

    Created the trivy-scan shared lib

--- vars/test.groovy
+++
@@ -1,9 +0,0 @@
-def call(Map config[:]){
-    def dockerImage = config.dockerImage ?: error('Image is required')
-    def dockerTag = config.dockerTag ?: 'latest'
-    def dockerFile = config.dokcerFile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    sh """
-    docker build -t ${dockerImage}:${dockerTag}  -f ${dockerFile} ${context}
-    """
-}
